74e9b42780a5dc9b76336a816dd6a2a72d893d94
philipjrosen <philipjrosen@gmail.com>
2013-08-27 14:22:09 -0700

    added templates for translated Messages and translations; added template helper, 'translations'; created the collections 'Messages' and 'Translations'; Populating translations on server startup

--- babelchat.js
+++ babelchat.js
@@ -1,19 +1,47 @@
 Rooms = new Meteor.Collection('rooms');
+Messages = new Meteor.Collection('messages');
+Translations = new Meteor.Collection('translations');
 
 if (Meteor.isClient) {
   Template.roomList.rooms = function () {
     return Rooms.find({},{sort: {Category: 1}});
   };
+
+  // Template.sourceMessages.messages = function() {
+  //   return Messagess.find();
+  // };
+
+  Template.translatedMessages.translations = function() {
+    return Translations.find();
+  };
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-   if(Rooms.find().count() === 0) {
-    Rooms.insert({roomName:'English'});
-    Rooms.insert({roomName:'Spanish'});
-    Rooms.insert({roomName:'French'});
-    Rooms.insert({roomName:'German'});
-  }
+    if(Rooms.find().count() === 0) {
+      Rooms.insert({roomName:'English'});
+      Rooms.insert({roomName:'Spanish'});
+      Rooms.insert({roomName:'French'});
+      Rooms.insert({roomName:'German'});
+    }
+    if(Translations.find().count() === 0) {
+      Translations.insert({
+        user: "John",
+        text: "Imaginate"
+      });
+      Translations.insert({
+        user: "Paul",
+        text: "Dame tu mano?"
+      });
+      Translations.insert({
+        user: "George",
+        text: "Algo"
+      });
+      Translations.insert({
+        user: "Ringo",
+        text: "Octupus"
+      });
+    }
   });
 }
 

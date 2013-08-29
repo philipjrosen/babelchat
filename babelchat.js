2d0ac65d2be18836956376d82e2a399557245e06
philipjrosen <philipjrosen@gmail.com>
2013-08-29 10:00:21 -0700

    moved startup code from Meteor.isServer to new file 'fixtures.js' in server folder

--- babelchat.js
+++ babelchat.js
@@ -94,13 +94,6 @@ if (Meteor.isClient) {
 //SERVER CODE//
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    if(Chats.find().count() === 0) {
-      Chats.insert({room:'English', target:'en'});
-      Chats.insert({room:'Spanish', target:'es'});
-      Chats.insert({room:'French',  target:'fr'});
-      Chats.insert({room:'German',  target:'de'});
-      Chats.insert({room:'Hebrew',  target:'iw'});
-      Chats.insert({room:'Italian', target:'it'});
-    }
-  });
+
+ });
 }

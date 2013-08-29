b6cecc556e10bb6b7f19cdce19917d6108f6f3d5
philipjrosen <philipjrosen@gmail.com>
2013-08-29 15:07:36 -0700

    in chats.js added logic to allow users to post messages only if logged in

--- collections/chats.js
+++ collections/chats.js
@@ -1,1 +1,9 @@
 Chats = new Meteor.Collection('chats');
+
+Chats.allow({
+  insert: function (userId, doc) { return !! userId; },
+  update: function (userId, doc) { return !! userId; },
+  //might want to give the user the ability to delete messages:
+  // remove: function () { return !! userId; },
+  // fetch:  function () { return !! userId; }
+});

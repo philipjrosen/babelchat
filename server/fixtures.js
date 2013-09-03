345c0c4824a8f029598dc707c95e6061bbe30ec2
philipjrosen <philipjrosen@gmail.com>
2013-09-03 11:36:05 -0700

    added css for the message output

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,8 +1,8 @@
 if(Chats.find().count() === 0) {
   Chats.insert({room:'English', target:'en'});
-	Chats.insert({room:'Spanish', target:'es'});
-	Chats.insert({room:'French',  target:'fr'});
-	Chats.insert({room:'German',  target:'de'});
-	Chats.insert({room:'Hebrew',  target:'iw'});
-    Chats.insert({room:'Italian', target:'it'});
+	// Chats.insert({room:'Spanish', target:'es'});
+	// Chats.insert({room:'French',  target:'fr'});
+	// Chats.insert({room:'German',  target:'de'});
+	// Chats.insert({room:'Hebrew',  target:'iw'});
+ //    Chats.insert({room:'Italian', target:'it'});
 }
